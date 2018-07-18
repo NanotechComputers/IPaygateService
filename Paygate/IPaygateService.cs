8e4bed574fd637c351ff373e1bd80e8bb0425d58
Kevin Deason <kevin@deason.co.za>
2018-07-18 19:35:09 +0200

    feat: Add Settlement and Refund functionality

--- Paygate/IPaygateService.cs
+++ Paygate/IPaygateService.cs
@@ -8,9 +8,9 @@ namespace Paygate
     {
         TransactionResponse CreateTransaction(CreateTransactionModel requestData);
         TransactionResponse<TUserdefined> CreateTransaction<TUserdefined>(CreateTransactionModel<TUserdefined> requestData) where TUserdefined : class;
-        
         bool VerifyTransaction(Dictionary<string, string> urlParams, string payRequestId);
-        
         TransactionResponse QueryTransaction(string paygateRequestId);
+        TransactionResponse SettleTransaction(string transactionId, int amount);
+        TransactionResponse RefundTransaction(string transactionId, int amount);
     }
 }
---
+++ Paygate/Infrastructure/SoapTemplates/SingleRefundRequestSoapXml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Paygate.Infrastructure.SoapTemplates
+{
+    internal static class SingleRefundRequestSoapXml
+    {
+        internal static string Get(string merchantId, string merchantSecret, string transactionId, int amount)
+        {
+            
+            if (string.IsNullOrEmpty(transactionId))
+            {
+                throw new NullReferenceException(nameof(transactionId)); 
+            }
+            
+            return $@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:pay=""http://www.paygate.co.za/PayHOST"">
+                        <soapenv:Header/>
+                        <soapenv:Body>
+                            <pay:SingleFollowUpRequest>
+                                <pay:RefundRequest>
+                                    <pay:Account>
+                                        <pay:PayGateId>{merchantId ?? ""}</pay:PayGateId>
+                                        <pay:Password>{merchantSecret ?? ""}</pay:Password>
+                                    </pay:Account>
+                                    <pay:TransactionId>{transactionId ?? ""}</pay:TransactionId>
+                                    <pay:Amount>{amount}</pay:Amount>
+                                </pay:RefundRequest>
+                            </pay:SingleFollowUpRequest>
+                        </soapenv:Body>
+                    </soapenv:Envelope>";
+        }
+    }
+}
