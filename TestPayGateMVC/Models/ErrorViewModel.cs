b87fe280500087a76a20fbd35403d6c76bbda0eb
Kevin Deason <kevin@deason.co.za>
2018-11-10 17:54:49 +0200

    chore: Remove Sample Projects to split into own git repositories

--- TestPayGateMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestPayGateMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
